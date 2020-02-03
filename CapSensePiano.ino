4c9d23bff551a72ed1848b5140a5c12479d63647
iamyunika <yupadhayaya08@gmail.com>
2020-02-03 15:06:53 -0600

    adding all files

---
+++ CapSensePiano.ino
@@ -0,0 +1,55 @@
+/*
+  Capacitive-Touch Arduino Keyboard Piano
+  
+  Plays piano tones through a buzzer when the user taps touch-sensitive piano "keys"
+  
+  Created  June 17 2019
+  by Yunika Upadhayaya
+*/
+
+#include <CapacitiveSensor.h>
+#include "pitches.h"
+
+#define COMMON_PIN      2    // The common 'send' pin for all keys
+#define BUZZER_PIN      A4   // The output pin for the piezo buzzer
+#define NUM_OF_SAMPLES  10   // Higher number whens more delay but more consistent readings
+#define CAP_THRESHOLD   300 // Capactive reading that triggers a note
+#define NUM_OF_KEYS     6    // Number of keys that are on the keyboard
+
+// This macro creates a capacitance "key" sensor object for each key on the piano keyboard:
+#define CS(Y) CapacitiveSensor(2, Y)
+
+// Each key corresponds to a note, which are defined here. Uncomment the scale that you want to use:
+int notes[]={NOTE_C4,NOTE_D4,NOTE_E4,NOTE_F4,NOTE_G4,NOTE_A4,NOTE_B4,NOTE_C5}; // C-Major scale
+//int notes[]={NOTE_A4,NOTE_B4,NOTE_C5,NOTE_D5,NOTE_E5,NOTE_F5,NOTE_G5,NOTE_A5}; // A-Minor scale
+//int notes[]={NOTE_C4,NOTE_DS4,NOTE_F4,NOTE_FS4,NOTE_G4,NOTE_AS4,NOTE_C5,NOTE_DS5}; // C Blues scale
+
+// Defines the pins that the keys are connected to:
+CapacitiveSensor keys[] = {CS(3), CS(4), CS(5), CS(6), CS(7), CS(8)};
+
+void setup() { 
+  // Turn off autocalibrate on all channels:
+  for(int i=0; i<6; ++i) {
+    keys[i].set_CS_AutocaL_Millis(0xFFFFFFFF);
+  }
+  // Set the buzzer as an output:
+  pinMode(BUZZER_PIN, OUTPUT); 
+}
+
+void loop() {    
+  // Loop through each key:
+  for (int i = 0; i < 6; ++i) {
+    // If the capacitance reading is greater than the threshold, play a note:
+    if(keys[i].capacitiveSensor(NUM_OF_SAMPLES) > CAP_THRESHOLD) {
+      tone(BUZZER_PIN, notes[i]); // Plays the note corresponding to the key pressed
+    }
+    else//If the capacitance reading is not greater than the threshold, do not play a note:
+    {
+      noTone(BUZZER_PIN);
+    }
+   
+   }
+    
+  }
+    
+    
